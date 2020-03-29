d23909fd551c6584a13d007a941f5536c820217e
yasukotelin <31115673+yasukotelin@users.noreply.github.com>
2020-03-29 13:47:07 +0900

    Add showing a result message in discard command

--- cmd/discard.go
+++ cmd/discard.go
@@ -1,11 +1,19 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/urfave/cli/v2"
 	"github.com/yasukotelin/git-ex/usecase/git"
 )
 
 // Discard executes the removing all changes from the HEAD
 func Discard(c *cli.Context) error {
-	return (&git.GitUseCase{}).Discard()
+	err := (&git.GitUseCase{}).Discard()
+	if err != nil {
+		fmt.Println("Discard failed.")
+		return err
+	}
+	fmt.Println("Discard successed.")
+	return nil
 }
